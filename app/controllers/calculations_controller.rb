b073510009ad3ea068c687b4b25245e0fb0dd425
Pierre Hage <phage@chicagobooth.edu>
2016-11-08 15:37:41 -0600

    Finito
    Done

--- app/controllers/calculations_controller.rb
+++ app/controllers/calculations_controller.rb
@@ -2,4 +2,27 @@ class CalculationsController < ApplicationController
   def instructions
     render("instructions.html.erb")
   end
+
+  def square_root
+  @number = params[:id].to_i
+  @sqrrt= Math.sqrt(@number)
+  end
+
+  def random
+  @first = params[:ids].to_i
+  @last = params[:ide].to_i
+  @rand = rand(@first..@last)
+  end
+
+  def payment
+  @interest = params[:int].to_i
+  @interestrate = @interest/10
+  @years = params[:yr].to_i
+  @amount = params[:amt].to_i
+  intdec= @interest/10
+  @payment= intdec * @amount / @years
+  end
+
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   get("/", { :controller => "calculations", :action => "instructions" })
+  get("/square_root/:id", { :controller => "calculations", :action => "square_root" })
+  get("/random/:ids/:ide", { :controller => "calculations", :action => "random" })
+  get("/payment/:int/:yr/:amt", { :controller => "calculations", :action => "payment" })
 end
